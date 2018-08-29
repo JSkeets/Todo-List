fc07921083f4998d49db6773803db54bc4e5d550
torreyhogan <42388214+torreyhogan@users.noreply.github.com>
2018-08-28 22:57:19 -0700

    Add files via upload

--- Todo List Project/assets/js/todoList.js
+++ Todo List Project/assets/js/todoList.js
@@ -1,56 +1,68 @@
 
 var input = document.querySelector("input");
-var listItems = document.querySelectorAll("li");
-var todo = document.querySelectorAll("todo");
+var listItems = document.getElementsByTagName("li");
+
+var trashBtn = document.getElementsByClassName("trashBtn");
+var list = document.getElementById("parent");
 
 
 // addEventListener added to todo list items
 for(var i = 0; i < listItems.length; i++) {
 
-	deleteBtn();
+	// deleteBtn();
 	strikeThrough();
+	deleteLine();
+}
+// addEventListener for Delete Button
 
+function deleteLine() {
+	let nested = listItems[i];
+	trashBtn[i].addEventListener("click", function() {
+		list.removeChild(nested);
+		// stop event propigation
+		event.stopPropagation();
+	});
 }
 
 // adds trashcan icon when mouseover todo item
 function deleteBtn() {
+	var changer = trashBtn[i]
 	listItems[i].addEventListener("mouseover", function() {
-		var textInside = this.textContent;
-		this.innerHTML = textInside + "<span class=\"deleteBtn\"><i class=\"fas fa-trash-alt\"></i></span>";
-
+		changer.innerHTML = "<i class=\"fas fa-trash-alt\">";
+		// this.innerHTML = textInside + "<span class=\"deleteBtn\"><i class=\"fas fa-trash-alt\"></i></span>";
 		// reset the colors after a short delay
-		setTimeout(function() {
-			this.innerHTML = textInside;
-		}, 100);
+		
+
+	});
+	listItems[i].addEventListener("mouseout", function(){
+		changer.innerHTML = "";
 	});
 }
 // line-through clicked items in list
 function strikeThrough() {
-	listItems[i].addEventListener("click", function() {
+	listItems[i].addEventListener("click", function(event) {
 		this.classList.toggle("strike");
+
 	});
 }
 
 
 
+// input.addEventListener("mouseover" , function() {
+// 	addLi();
+// });
 
 
-input.addEventListener("click" , function() {
-	addLi();
-});
+// function addLi() {
+// 	// new element
+// 	var newLi = document.createElement("li");
+// 	// content for new element
+// 	var newContent = document.createTextNode("Hi there and greetings");
+	
+// 	// add the text node to the newly created div
+	
 
-
-function addLi() {
-	// new element
-	var newLi = document.createElement("li");
-	// content for new element
-	var newContent = document.createTextNode("Hi there and greetings");
-	// add the text node to the newly created div
-	newLi.appendChild(newContent);
-
-	// add the newly created element and its content into the DOM
-	var allLi = document.querySelectorAll("li");
-	var lastLi = allLi[allLi.length - 1];
-	lastLi.insertBefore(newLi, lastLi);
-}
+// 	// add the newly created element and its content into the DOM
+// 	list.appendChild(newContent);
+// }
 
